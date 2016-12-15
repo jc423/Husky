e5affda3d872680fae1aed6a0f6cffe7444e4679
Crawford, Joseph (CORP) <Joseph.Crawford@adp.com>
2016-12-15 13:07:12 -0500

    add minkoski distance and do higher ordered shit

--- src/Distance.hs
+++ src/Distance.hs
@@ -1,19 +1,22 @@
-
 module Distance where
 
 type DistanceFunction a  = [a] -> [a] -> a
 
-euclidean :: (Ord a,Floating a) => DistanceFunction a
-euclidean a b = sqrt (sumOfSquare a b)
+minkowski :: (Ord a, Floating a) => Int -> DistanceFunction a
+minkowski p xs ys = (**) (sum $ zipWith (\x y -> abs(x - y) ^ p) xs ys) (1.0 / fromIntegral p)
 
-manhattan :: Floating a => DistanceFunction a
-manhattan a b = sum $ zipWith (\x y -> abs(x - y)) a b
+euclidean :: (Ord a,Floating a) => DistanceFunction a
+euclidean xs ys = minkowski 2 xs ys
+
+manhattan :: (Ord a, Floating a) => DistanceFunction a
+manhattan xs ys = minkowski 1 xs ys
 
 -- adding  to -1 is to account for cosine similarity of 1 meaning same orientation, -1 opposite and 0 orthogonal
 cosineDistance ::(Floating a) => DistanceFunction a
-cosineDistance a b = abs $ (+) (-1) $ (/) (sum $ zipWith (\x y -> x * y) a b) ((magnitude a) * (magnitude b))
+cosineDistance xs ys = abs $ (+) (-1) $ (/) (sum $ zipWith (\x y -> x * y) xs ys) ((magnitude xs) * (magnitude ys))
 
-sumOfSquare a b = sum $ zipWith (\x y -> (x - y)^2) a b
-magnitude a = sqrt $ sum $ zipWith (\x y -> x * y) a a
+
+sumOfSquare xs ys = sum $ zipWith (\x y -> (x - y)^2) xs ys
+magnitude xs = sqrt $ sum $ zipWith (\x y -> x * y) xs xs
 
 

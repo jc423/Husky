21942ab2584c116f176d53a4fd61a41d30a4cb8c
Crawford, Joseph (CORP) <Joseph.Crawford@adp.com>
2016-12-15 01:03:40 -0500

    fix cosine distance to account for negative values

--- src/Distance.hs
+++ src/Distance.hs
@@ -3,7 +3,7 @@ module Distance where
 
 type DistanceFunction a  = [a] -> [a] -> a
 
-euclidean :: Floating a => DistanceFunction a
+euclidean :: (Ord a,Floating a) => DistanceFunction a
 euclidean a b = sqrt (sumOfSquare a b)
 
 manhattan :: Floating a => DistanceFunction a
@@ -11,7 +11,7 @@ manhattan a b = sum $ zipWith (\x y -> abs(x - y)) a b
 
 -- adding  to -1 is to account for cosine similarity of 1 meaning same orientation, -1 opposite and 0 orthogonal
 cosineDistance ::(Floating a) => DistanceFunction a
-cosineDistance a b = (+) (-1) $ (/) (sum $ zipWith (\x y -> x * y) a b) ((magnitude a) * (magnitude b))
+cosineDistance a b = abs $ (+) (-1) $ (/) (sum $ zipWith (\x y -> x * y) a b) ((magnitude a) * (magnitude b))
 
 sumOfSquare a b = sum $ zipWith (\x y -> (x - y)^2) a b
 magnitude a = sqrt $ sum $ zipWith (\x y -> x * y) a a

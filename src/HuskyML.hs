6e21a845a0c32c986236a11bfb2dcd4afd59de7d
Crawford, Joseph (CORP) <Joseph.Crawford@adp.com>
2016-12-20 21:37:15 -0500

    support mixed features of other types

--- src/HuskyML.hs
+++ src/HuskyML.hs
@@ -11,9 +11,13 @@ data Feature = FString String | FInt Int | FDouble Double | FChar Char deriving 
 instance Attribute Feature where
   diff (FString s1) (FString s2) = fromIntegral $ (length s1) - (length s2)
   diff (FInt s1) (FInt s2) = fromIntegral $ s1 -  s2
+  diff (FDouble d1) (FDouble d2) = d1 - d2
+  diff (FChar c1) (FChar c2) = fromIntegral $ fromEnum c1 - fromEnum c2
   diff _ _ = error "unable to diff a0 a1"
-  times (FString s1) (FString s2) = 0.0
+  times (FString s1) (FString s2) = fromIntegral $ (length s1) * (length s2)
   times (FInt s1) (FInt s2) = fromIntegral $ s1 * s2
+  times (FDouble d1) (FDouble d2) = d1 * d2
+  times (FChar c1) (FChar c2) = fromIntegral $ fromEnum c1 *  fromEnum c2
   times _ _ = error "unable to times a0 a1"
 
 
--- test/DistanceSpec.hs
+++ test/DistanceSpec.hs
@@ -1,6 +1,7 @@
 module DistanceSpec (main, spec) where
 
 import Test.Hspec
+import Control.Exception (evaluate)
 import Distance
 import HuskyML
 
@@ -12,6 +13,9 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "Distance" $ do
+    it "euclidean distance should throw when two vectors have mismatched corresponding types" $ do
+      evaluate (Distance.euclidean [FInt 1, FChar 'b'] [FInt 4, FInt 6]) `shouldThrow` anyException
+    
     it "euclidean distance should return euclidean distance between two vectors for positive values" $ do
       Distance.euclidean [FInt 1, FInt 2] [FInt 4, FInt 6] `shouldBe` 5.0
 
@@ -20,6 +24,9 @@ spec = do
 
     it "euclidean distance should return euclidean distance between two vectors for mix of positive and negative values" $ do
       Distance.euclidean [FInt (-1),FInt 2] [FInt 7, FInt 8] `shouldBe` 10.0
+
+    it "euclidean distance should return euclidean distance between two vectors for mixed feature types" $ do
+      Distance.euclidean [FInt 1, FString "jo", FChar 'a', FDouble 1.0 ] [FInt 4, FString "crawfo", FChar 'a', FDouble 1.0] `shouldBe` 5.0
 
     it "manhattan distance should return manhattan distance between two vectors for positive values" $ do
       Distance.manhattan [FInt 0, FInt 0] [FInt 2,FInt 2] `shouldBe` 4.0
@@ -30,6 +37,9 @@ spec = do
     it "manhattan distance should return manhattan distance between two vectors for mix of positive and negative values" $ do
       Distance.manhattan [FInt (-1),FInt 0] [FInt 2,FInt (-4)] `shouldBe` 7.0
 
+    it "manhattan distance should return manhattan distance between two vectors for mixed feature types" $ do
+      Distance.manhattan [FChar 'd',FInt 0] [FChar 'a',FInt (-4)] `shouldBe` 7.0
+
     it "cosine distance should return absolute value of 1 - cosine similarity between two vectors for positive values" $ do
       Distance.cosineDistance [FInt 0,FInt 1] [FInt 0,FInt 2] `shouldBe` 0.0
 
@@ -39,3 +49,5 @@ spec = do
     it "cosine distance should return absolute value of 1 - cosine similarity between two vectors for mix of positive  and negative values" $ do
       Distance.cosineDistance [FInt 1,FInt 1] [FInt (-1),FInt (-1)] - 2.0 `shouldSatisfy` (< 0.0000001)
 
+    it "cosine distance should return absolute value of 1 - cosine similarity between two vectors for mixed feature types" $ do
+      Distance.cosineDistance [FInt 1,FString "Joseph"] [FInt (-1),FString "Violet"] - 2.0 `shouldSatisfy` (< 0.0000001)
